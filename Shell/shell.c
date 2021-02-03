c11dee9c9d7ca65260efc54c2afc602c653f515d
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-02-03 11:25:02 -0600

    enter

--- Shell/shell.c
+++ Shell/shell.c
@@ -1,7 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "parse.c"
 
-int main(int argc, char *argv[]) {
-    
+int main() {
+
+    char fnctn;
+    scanf("%c",&fnctn);
+
+    switch (fnctn)
+    {
+        case'exit':
+            return 0;
+            break;
+        case'bg':
+            printf("hello world (pid:%d)\n", getpid());
+            int rc = fork();
+            if (rc < 0)
+            {
+                //fork failed
+                fprintf(stderr, "fork failed\n");
+            } else if (rc == 0) {
+                //child (new process)
+                printf("hello, I am child (pid:%d)\n", getpid());
+            } else {
+                //parent goes down this path (main)
+                printf("hello, I am parent of %d (pid:%d)\n",rc,getpid());
+            }
+            break;
+        case 'ls':
+            
+            break;
+        default:
+            printf("Invalid command\n");
+            break;
+    }
+    return 0;
 }
