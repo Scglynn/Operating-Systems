1187b16a3741ca975b0a838dee97488b3b94b79b
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-02-02 21:47:01 -0600

    enter

---
+++ Shell/exec.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+
+    printf("hello world (pid:%d)\n", getpid());
+    int rc = execlp("ls", "ls", NULL);
+    if (rc < 0)
+        fprintf(stderr, "exec failed\n");
+    else
+        printf("it worked!\n");
+}
---
+++ Shell/run.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    printf("hello world (pid:%d)\n", getpid());
+
+    int rc = fork();
+    if (rc < 0)
+    {
+        // fork failed
+        fprintf(stderr, "fork failed\n");
+    }
+    else if (rc == 0) {
+        // child -- "restart"
+        execlp("ls","ls", NULL);
+        fprintf(stderr, "exec failed\n");
+    } else{
+        //parent -- wait and report
+        wait(NULL);
+        printf("finished process %d (pid:%d)\n",rc,getpid());
+    }
+}
