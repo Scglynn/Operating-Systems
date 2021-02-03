3168f5d28ec86f1916b4b3beed0acb675d52df88
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-02-02 20:47:20 -0600

    Enter

--- Shell/fork.c
+++ Shell/fork.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int
 main(int argc, char *argv[])
 {
-    printf("hello world (pid:%d)\n", (int) getpid());
+    printf("hello world (pid:%d)\n", getpid());
     int rc = fork();
-    if (rc < 0) {
-        // fork failed; exit
+    if (rc < 0)
+    {
+        //fork failed
         fprintf(stderr, "fork failed\n");
-        exit(1);
-    } else if (rc == 0) {
-        // child (new process)
-        printf("hello, I am child (pid:%d)\n", (int) getpid());
+    } else if (rc == 0)
+    {
+        //child (new process)
+        printf("hello, I am child (pid:%d)\n", getpid());
     } else {
-        // parent goes down this path (original process)
-        printf("hello, I am parent of %d (pid:%d)\n",
-	       rc, (int) getpid());
+        //parent goes down this path (main)
+        printf("hello, I am parent of %d (pid:%d)\n",rc,getpid());
     }
-    return 0;
 }
