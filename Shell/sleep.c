bae52cc2661dcec93edd9bef8144d74b172cbd27
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-02-09 13:54:11 -0600

    enter

---
+++ Shell/sleep.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "user.h"
+
+int
+main(int argc, char const *argv[])
+{
+    int ticks = 50;
+    if (argc > 2)
+    {
+        printf(1,"usage: sleep [ticks]\n");
+        exit();
+    }
+    if(argc == 2)
+        ticks = atoi(argv[1]);
+
+    sleep(ticks);
+    exit();
+}
