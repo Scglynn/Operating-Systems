4e37e2eb2510593a94c02342c7a90b91b4d85d35
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-02-09 17:05:06 -0600

    updated

--- Shell/shell.c
+++ Shell/shell.c
@@ -1,58 +1,77 @@
-#include "parse.h"
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fcntl.h"
+#include "parse.h"
+
 int 
 main(int argc, char* argv[]) {
 
     char* input = (char*)malloc(1024);
-    char* output = (char*)malloc(1024);
+   
     command cmd;
     
-
+ 
     
 
     while(1) {
         printf(1,"shell> ",getpid());
         gets(input,1024);
-        int rtrn_c = parse(input,&cmd);
-        int rc = rtrn_c;
+        int rc = parse(input,&cmd);
         rc = fork();
         
+        
+        
+
+        //checking to see if the command in shell is 'exit'
         if (strcmp(input, cmd.argv[0]="exit")==0)
         {
             wait();
         }
+        
+        //checking to see if the command in shell is 'echo'
         if (strcmp(input,cmd.argv[0]="echo")==0)
         {
-            if (rc<0)
+            
+            if (rc < 0)
             {
+                
                 printf(2,"fork failed");
+
             }else if (rc == 0)
             {
-                exec(input,cmd.argv);
-                printf(2,"exec failed\n");
+                exec(cmd.argv[0],cmd.argv);
+                if (cmd.input)
+                {
+                    close(0);
+                    open(cmd.input, O_CREATE | O_RDONLY);
+                }
+                if (cmd.output)
+                {
+                    close(1);
+                    open(cmd.output, O_CREATE | O_WRONLY);
+
+                }
                 
             }else{
-                
-                
                 wait();
+                //printf(1,"hello, I am parent of %d (pid:%d)\n",rc,getpid());
             }
-        }
-        if (strcmp(input,cmd.argv[0]="bg")==0)
-        {
-            if (rc<0)
+            if (strcmp(input,cmd.argv[0]="echo")==0)
             {
-                printf(2,"fork failed");
-            }else if (rc == 0)
-            {
-                exec(input,cmd.argv);
-                printf(2,"exec failed\n");
-                
-            }else{
-                
-                
-                wait();
+
+                if (cmd.input)
+                    {
+                        close(0);
+                        open(cmd.input,O_CREATE | O_RDONLY);
+                    }
+                    if (cmd.output)
+                    {
+                        close(1);
+                        open(cmd.output,O_CREATE | O_WRONLY);
+
+                    }
+                    exec(cmd.argv[0],cmd.argv);
             }
         }
         
@@ -68,9 +87,10 @@ main(int argc, char* argv[]) {
                 printf(2,"exec failed\n");
             }else{
                 wait();
-                //printf(1,"finished process %d (pid:%d)\n",rc,getpid());
+                printf(1,"finished process %d (pid:%d)\n",rc,getpid());
             }
         }
+
         
 
         
