39eb41fa1cfa98ce94b5a9bf471680aa6bc806e0
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-02-26 13:28:52 -0600

    enter

--- Decryptor/t.cpp
+++ Decryptor/t.cpp
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-
 int f(int x)
 {
-    if(x<2)return x;
+    if(x<2) return x;
     return f(x-1)+f(x-2);
-
 }
 
 int main()
 {
-    unsigned char *p = (unsigned char *)&f;
-    //unsigned char byte = 7;
+    unsigned char *p =(unsigned char *) &f;
+    //unsigned char byte = *p;
     printf("%02x ",*p);
     printf("%02x ",p[1]);
-    p[1]=103;
 }
