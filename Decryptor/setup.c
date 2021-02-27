917e48765025d8225a14a3eca8317b54d7097ab1
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-02-27 14:20:42 -0600

    passed test case one

--- Decryptor/setup.c
+++ Decryptor/setup.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 static int shift;
 static unsigned password_hash;
 static char * message;
+static char * password;
 
 extern unsigned hash(char *str);
 extern int check(char *password);
@@ -13,19 +15,24 @@ extern void decrypt(char *password,char *message);
 void setup()
 {
     static unsigned char encoded[] = {0xd9,0x24,0xad,0xe7,0xa2,0x61,0xa0,0xf9,0xeb,0x2f,0xe6,0xff,0xae,0x38,0xae,0xfe,0xae,0x32,0xac,0xea,0xfc,0x35,0xe0};
+    
     shift = 8;
     password_hash = 0x41c18b8e;
     message = (char *)encoded;
 
-    if (!check((char *)password_hash))
-        hash((char *)password_hash) == password_hash;
+    if(!check(password))
+    {
+        hash(password);
+        decrypt(password, message);
+    }
     else
     {
-        decrypt((char *)password_hash,message);
+        decrypt(password,message);
     }
-    
-    
-    
+    if (check(password) == true)
+    {
+        decrypt(password, message);
+    }
     
 
 }
