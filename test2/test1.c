6234fb35113316af5b433ae2c40ac0df2d7f43a2
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-03-02 15:47:18 -0600

    enter

---
+++ test2/test1.c
@@ -0,0 +1,8 @@
+int main(int argc, int ** argv){
+    // char * p = (char *)&main;
+    // int x = *p;
+    char * p = (char *)&main;
+    *p = 5;
+    // char * p = (char *)&main;
+    // int x = *p;
+}
