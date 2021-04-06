76de27950952b07d7918cd9f531f85c6a6b9ebdf
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-04-05 22:06:58 -0500

    transpose fixed

--- matrix_Manipulation/transpose.c
+++ matrix_Manipulation/transpose.c
@@ -8,43 +8,44 @@ struct matrix {
     int column;
 };
 
-int val = 0;
+int value_holder = 0;
 int holder = 0;
 int count = 0;
 
 int main(int argc, char *argv[]) 
 {
     struct matrix matrices;
-    // char * input_file;
-    // char * output_file;
+    char * input_file;
+    char * output_file;
     
     if (argc !=3)
     {
         printf("you are missing an input or output file name\n");
-        exit(1);
+        return 1;
+        //exit(1);
     }
 
-    // input_file = argv[1];
-    // output_file = argv[2];
+    input_file = argv[1];
+    output_file = argv[2];
 
-    int fd_read = open(argv[1],O_RDONLY, 0666);
+    int fd_read = open(input_file,O_RDONLY, 0666);
     if (fd_read < 0)
     {
-        printf("Failed to read and open the file");
+        printf("Failed to read and open the file\n");
         return 1;
         //exit(1);
     }
 
     //this will write the transpose matrices to the outputfile 
-    int out = open(argv[2], O_CREAT|O_TRUNC|O_WRONLY,0666);
+    int out = open(output_file, O_CREAT|O_TRUNC|O_WRONLY,0666);
     if (out < 0)
     {
-        printf("Failed to create and open the file");
+        printf("Failed to create and open the file\n");
         return 1;
         //exit(1);
     }
 
-    int *x =(int *)malloc(1000000 * sizeof(*x));
+    int *x =(int *)malloc(100 /*(matrices.row * matrices.column)*/ * sizeof(*x));
  
 
     //stores(reads) in the matrix with all of it's elements
@@ -55,18 +56,23 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    holder = read(fd_read, x, (matrices.row * matrices.column)*sizeof(matrices));
+    holder = read(fd_read, x, (matrices.row * matrices.column) * sizeof(matrices));
 
 
     //transpose the matrix
+    //first store the cuurent row size
     int z = matrices.row;
+
+    //now make the new row size equal to column
     matrices.row = matrices.column;
+
+    //now place the past row size to column size
     matrices.column = z;
 
     //the new transpose matrix
     int transpose_matrix[matrices.row][matrices.column];
 
-    for (int i = 0; i < matrices.row; i++)
+    for (int i = 0; i < matrices.column; i++)
     {
         for (int j = 0; j < matrices.row; j++, count++)
         {
@@ -74,13 +80,13 @@ int main(int argc, char *argv[])
         }
         
     }
-    
 
 
+    //write the new transposed matrix into the output file
     if(write(out,&matrices,sizeof(matrices)) != sizeof(matrices)) goto bad1;
 
     //places the elements
-    val = write(out, *transpose_matrix,(matrices.row * matrices.column)*sizeof(int));
+    value_holder = write(out, *transpose_matrix,(matrices.row * matrices.column)*sizeof(int));
 
 
     free(x);
